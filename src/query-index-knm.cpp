c8dcc2ace26922d271715968db4f95607aa52d1e
eehsan <ehsan.shareghi@gmail.com>
2015-04-09 22:10:51 +1000

    stable version ready for test

--- src/query-index-knm.cpp
+++ src/query-index-knm.cpp
@@ -72,46 +72,34 @@ parse_args(int argc, const char* argv[])
 void print(const std::vector<uint64_t>::iterator& pattern_begin,
            const std::vector<uint64_t>::iterator& pattern_end)
 {
-        for(auto it = pattern_begin;it!=pattern_end;it++)
-        {
-                cout<<*it<<" ";
-        }
-        cout<<endl;
-}
-
-void print(const std::vector<uint64_t>::reverse_iterator& pattern_rbegin,
-           const std::vector<uint64_t>::reverse_iterator& pattern_rend)
-{
-        for(auto it = pattern_rend;it!=pattern_rbegin;it++)
-        {
-                cout<<*it<<" ";
-        }
-        cout<<endl;
+    for (auto it = pattern_begin; it != pattern_end; it++) {
+        cout << *it << " ";
+    }
+    cout << endl;
 }
 
 // computes N_1+( * abc ) equivalent to computing N_1+ ( cba *) in the reverse suffix tree
 template <class t_idx>
-int N1PlusBack(const t_idx& idx, const uint64_t& lb_rev, const uint64_t& rb_rev, int patrev_size, bool check_for_EOS=true)
+int N1PlusBack(const t_idx& idx, const uint64_t& lb_rev, const uint64_t& rb_rev, int patrev_size, bool check_for_EOS = true)
 {
     uint64_t c = 0;
     auto node = idx.m_cst_rev.node(lb_rev, rb_rev);
-    uint64_t deg = idx.m_cst_rev.degree(node);
     if (patrev_size == idx.m_cst_rev.depth(node)) {
-        c = deg;
+        c = idx.m_cst_rev.degree(node);
         if (check_for_EOS) {
-        	auto w = idx.m_cst_rev.select_child(node, 1);
-        	uint64_t symbol = idx.m_cst_rev.edge(w, patrev_size + 1);
-        	if (symbol == 1)
-            		c = c - 1;
-	}
+            auto w = idx.m_cst_rev.select_child(node, 1);
+            uint64_t symbol = idx.m_cst_rev.edge(w, patrev_size + 1);
+            if (symbol == 1)
+                c = c - 1;
+        }
     } else {
-	if (check_for_EOS) {
-        	uint64_t symbol = idx.m_cst_rev.edge(node, patrev_size + 1); 
-        	if (symbol != 1) 
-        	    c = 1;
-	} else {
-		c = 1;
-	}
+        if (check_for_EOS) {
+            uint64_t symbol = idx.m_cst_rev.edge(node, patrev_size + 1);
+            if (symbol != 1)
+                c = 1;
+        } else {
+            c = 1;
+        }
     }
     return c;
 }
@@ -123,110 +111,111 @@ double discount(const t_idx& idx)
     return D;
 }
 
-//  Computes N_1+( * ab * ) 
+//  Computes N_1+( * ab * )
 //  n1plus_front = value of N1+( * abc ) (for some following symbol 'c')
 //  if this is N_1+( * ab ) = 1 then we know the only following symbol is 'c'
 //  and thus N1+( * ab * ) is the same as N1+( * abc ), stored in n1plus_front
 template <class t_idx>
-uint64_t N1PlusFrontBack(const t_idx& idx, 
-                     const uint64_t& lb, const uint64_t& rb, 
-                     const uint64_t n1plus_front,
-                     std::vector<uint64_t>::reverse_iterator pattern_rbegin,
-                     std::vector<uint64_t>::reverse_iterator pattern_rend,
-		     bool check_for_EOS=true)
+uint64_t N1PlusFrontBack(const t_idx& idx,
+                         const uint64_t& lb, const uint64_t& rb,
+                         const uint64_t n1plus_front,
+                         const std::vector<uint64_t>::iterator& pattern_begin,
+                         const std::vector<uint64_t>::iterator& pattern_end,
+                         bool check_for_EOS = true)
 {
     // ASSUMPTION: lb, rb already identify the suffix array range corresponding to 'pattern' in the forward tree
     // ASSUMPTION: pattern_begin, pattern_end cover just the pattern we're interested in (i.e., we want N1+ dot pattern dot)
-    int pattern_size = std::distance(pattern_rbegin,pattern_rend);
+    int pattern_size = std::distance(pattern_begin, pattern_end);
     auto node = idx.m_cst.node(lb, rb);
     uint64_t back_N1plus_front = 0;
-
+    uint64_t lb_rev_prime = 0, rb_rev_prime = idx.m_cst_rev.size() - 1;
+    uint64_t lb_rev_stored = 0, rb_rev_stored = 0;
+    // this is a full search for the pattern in reverse order in the reverse tree!
+    for (auto it = pattern_begin; it != pattern_end and lb_rev_prime <= rb_rev_prime;) {
+        backward_search(idx.m_cst_rev.csa,
+                        lb_rev_prime, rb_rev_prime,
+                        *it,
+                        lb_rev_prime, rb_rev_prime);
+        it++;
+    }
     // this is when the pattern matches a full edge in the CST
     if (pattern_size == idx.m_cst.depth(node)) {
         auto w = idx.m_cst.select_child(node, 1);
-
         int root_id = idx.m_cst.id(idx.m_cst.root());
-    	while (idx.m_cst.id(w) != root_id) {
-        	uint64_t symbol = idx.m_cst.edge(w, pattern_size + 1);
-        	if (symbol != 1 || !check_for_EOS) {
-		    uint64_t lb_rev_prime = 0, rb_rev_prime = idx.m_cst_rev.size()-1;//full interval - very inefficient
-		    // first step: find the symbol to the right
-		    // (which is first in the reverse order)
-		   backward_search(idx.m_cst_rev.csa, 
-				   lb_rev_prime, rb_rev_prime, 
-				   symbol,
-				   lb_rev_prime, rb_rev_prime);  
-		// this is a full search for the pattern!
-		   backward_search(idx.m_cst_rev.csa, 
-		                   lb_rev_prime, rb_rev_prime, 
-				   pattern_rbegin, 
-				   pattern_rend,
-				   lb_rev_prime, rb_rev_prime);  
-        	    back_N1plus_front += N1PlusBack(idx, lb_rev_prime, rb_rev_prime, pattern_size+1, check_for_EOS);
-       		}
-       		w = idx.m_cst.sibling(w);
-    	}         
-    	return back_N1plus_front;
+        while (idx.m_cst.id(w) != root_id) {
+            lb_rev_stored = lb_rev_prime;
+            rb_rev_stored = rb_rev_prime;
+            uint64_t symbol = idx.m_cst.edge(w, pattern_size + 1);
+            if (symbol != 1 || !check_for_EOS) {
+                // find the symbol to the right
+                // (which is first in the reverse order)
+                backward_search(idx.m_cst_rev.csa,
+                                lb_rev_stored, rb_rev_stored,
+                                symbol,
+                                lb_rev_stored, rb_rev_stored);
+
+                back_N1plus_front += N1PlusBack(idx, lb_rev_stored, rb_rev_stored, pattern_size + 1, check_for_EOS);
+            }
+            w = idx.m_cst.sibling(w);
+        }
+        return back_N1plus_front;
     } else {
         // special case, only one way of extending this pattern to the right
-	return n1plus_front;
+        return n1plus_front;
     }
 }
 
-// Computes N_1+( abc * ) 
+// Computes N_1+( abc * )
 template <class t_idx>
-uint64_t N1PlusFront(const t_idx& idx, 
-                     const uint64_t& lb, const uint64_t& rb, 
+uint64_t N1PlusFront(const t_idx& idx,
+                     const uint64_t& lb, const uint64_t& rb,
                      std::vector<uint64_t>::iterator pattern_begin,
                      std::vector<uint64_t>::iterator pattern_end,
-	             bool check_for_EOS=true)
+                     bool check_for_EOS = true)
 {
     // ASSUMPTION: lb, rb already identify the suffix array range corresponding to 'pattern' in the forward tree
     auto node = idx.m_cst.node(lb, rb);
-    int pattern_size = std::distance(pattern_begin,pattern_end);
+    int pattern_size = std::distance(pattern_begin, pattern_end);
     uint64_t N1plus_front = 0;
     if (pattern_size == idx.m_cst.depth(node)) {
         auto w = idx.m_cst.select_child(node, 1);
         N1plus_front = idx.m_cst.degree(node);
         if (check_for_EOS) {
-	    uint64_t symbol = idx.m_cst.edge(w, pattern_size + 1);
-	    if (symbol == 1) {
-		N1plus_front = N1plus_front - 1;
-	    }
-	}
+            uint64_t symbol = idx.m_cst.edge(w, pattern_size + 1);
+            if (symbol == 1) {
+                N1plus_front = N1plus_front - 1;
+            }
+        }
         return N1plus_front;
     } else {
         if (check_for_EOS) {
-	    uint64_t symbol = idx.m_cst.edge(node, pattern_size + 1);
-	    if (symbol != 1) {
-		N1plus_front = 1;
-	    }
-	}
-	return N1plus_front;
+            uint64_t symbol = idx.m_cst.edge(node, pattern_size + 1);
+            if (symbol != 1) {
+                N1plus_front = 1;
+            }
+        }
+        return N1plus_front;
     }
 }
 
 template <class t_idx>
-double highestorder(const t_idx& idx, 
-		    const std::vector<uint64_t>::iterator& pattern_begin,
+double highestorder(const t_idx& idx,
+                    const std::vector<uint64_t>::iterator& pattern_begin,
                     const std::vector<uint64_t>::iterator& pattern_end,
-                    const std::vector<uint64_t>::reverse_iterator& pattern_rbegin,
-                    const std::vector<uint64_t>::reverse_iterator& pattern_rend,
- 	            uint64_t& lb, uint64_t& rb,
+                    uint64_t& lb, uint64_t& rb,
                     uint64_t& lb_rev, uint64_t& rb_rev, uint64_t& char_pos, uint64_t& d)
 {
-    double backoff_prob = pkn(idx, pattern_begin+1, pattern_end,
-                              pattern_rbegin, pattern_rend-1,
- 			      lb, rb,
-			      lb_rev, rb_rev, char_pos,d);
-    auto node = idx.m_cst_rev.node(lb_rev,rb_rev);
+    double backoff_prob = pkn(idx, pattern_begin + 1, pattern_end,
+                              lb, rb,
+                              lb_rev, rb_rev, char_pos, d);
+    auto node = idx.m_cst_rev.node(lb_rev, rb_rev);
     uint64_t denominator = 0;
     uint64_t c = 0;
-    cout<<"SYMBOL="<<*pattern_begin<<" d="<<d<<endl;
-    if(forward_search(idx.m_cst_rev, node, d, *pattern_begin, char_pos)>0){
-	lb_rev = idx.m_cst_rev.lb(node);
-    	rb_rev = idx.m_cst_rev.rb(node);
-    	c = rb_rev - lb_rev + 1;
+    cout << "SYMBOL=" << *pattern_begin << " d=" << d << endl;
+    if (forward_search(idx.m_cst_rev, node, d, *pattern_begin, char_pos) > 0) {
+        lb_rev = idx.m_cst_rev.lb(node);
+        rb_rev = idx.m_cst_rev.rb(node);
+        c = rb_rev - lb_rev + 1;
     }
 
     double D = discount(idx);
@@ -237,23 +226,23 @@ double highestorder(const t_idx& idx,
     }
 
     uint64_t N1plus_front = 0;
-    if(backward_search(idx.m_cst.csa, lb, rb, *pattern_begin, lb, rb)>0){
-	denominator = rb - lb + 1;
+    if (backward_search(idx.m_cst.csa, lb, rb, *pattern_begin, lb, rb) > 0) {
+        denominator = rb - lb + 1;
         N1plus_front = N1PlusFront(idx, lb, rb, pattern_begin, pattern_end - 1);
-    }else{
+    } else {
         cout << "---- Undefined fractional number XXXZ - Backing-off ---" << endl;
-        return backoff_prob; 
+        return backoff_prob;
     }
 
     double output = (numerator / denominator) + (D * N1plus_front / denominator) * backoff_prob;
     cout
-    << " Highest Order" << endl
-    << " N1plus_front is: " << N1plus_front << endl
-    << " D is: " << D << endl
-    << " numerator is: " << numerator << endl 
-    << " denomiator is: " << denominator << endl
-    << " Highest Order probability " << output << endl
-    << "------------------------------------------------" << endl;
+        << " Highest Order" << endl
+        << " N1plus_front is: " << N1plus_front << endl
+        << " D is: " << D << endl
+        << " numerator is: " << numerator << endl
+        << " denomiator is: " << denominator << endl
+        << " Highest Order probability " << output << endl
+        << "------------------------------------------------" << endl;
     return output;
 }
 
@@ -261,21 +250,18 @@ template <class t_idx>
 double lowerorder(const t_idx& idx,
                   const std::vector<uint64_t>::iterator& pattern_begin,
                   const std::vector<uint64_t>::iterator& pattern_end,
-                  const std::vector<uint64_t>::reverse_iterator& pattern_rbegin,
-                  const std::vector<uint64_t>::reverse_iterator& pattern_rend,
                   uint64_t& lb, uint64_t& rb,
                   uint64_t& lb_rev, uint64_t& rb_rev, uint64_t& char_pos, uint64_t& d)
 {
-    double backoff_prob = pkn(idx, pattern_begin+1, pattern_end,
-                              pattern_rbegin, pattern_rend-1,
+    double backoff_prob = pkn(idx, pattern_begin + 1, pattern_end,
                               lb, rb,
-                              lb_rev, rb_rev, char_pos,d);
-    
+                              lb_rev, rb_rev, char_pos, d);
+
     uint64_t c = 0;
-    auto node = idx.m_cst_rev.node(lb_rev,rb_rev);
+    auto node = idx.m_cst_rev.node(lb_rev, rb_rev);
     int pattern_size = std::distance(pattern_begin, pattern_end);
-    print(pattern_begin,pattern_end);
-    if(forward_search(idx.m_cst_rev, node, d , *(pattern_begin), char_pos)>0){
+    print(pattern_begin, pattern_end);
+    if (forward_search(idx.m_cst_rev, node, d, *(pattern_begin), char_pos) > 0) {
         lb_rev = idx.m_cst_rev.lb(node);
         rb_rev = idx.m_cst_rev.rb(node);
         c = N1PlusBack(idx, lb_rev, rb_rev, pattern_size);
@@ -289,88 +275,84 @@ double lowerorder(const t_idx& idx,
 
     uint64_t N1plus_front = 0;
     uint64_t back_N1plus_front = 0;
-    if(backward_search(idx.m_cst.csa, lb, rb,*(pattern_begin) , lb, rb)>0){//TODO CHECK: what happens to the bounds when this is false?
-	print(pattern_rbegin, pattern_rend);
-        back_N1plus_front = N1PlusFrontBack(idx, lb, rb, c, pattern_rbegin, pattern_rend);
-	N1plus_front = N1PlusFront(idx, lb, rb, pattern_begin, pattern_end-1);
-    }else{
+    if (backward_search(idx.m_cst.csa, lb, rb, *(pattern_begin), lb, rb) > 0) { //TODO CHECK: what happens to the bounds when this is false?
+        back_N1plus_front = N1PlusFrontBack(idx, lb, rb, c, pattern_begin, pattern_end - 1);
+        N1plus_front = N1PlusFront(idx, lb, rb, pattern_begin, pattern_end - 1);
+    } else {
         cout << "---- Undefined fractional number XXXZ - Backing-off ---" << endl;
         return backoff_prob;
     }
 
     d++;
     double output = (numerator / back_N1plus_front) + (D * N1plus_front / back_N1plus_front) * backoff_prob;
-    cout 
-    << "Lower Order" << endl
-    << " N1plus_front is: " << N1plus_front << endl
-    << " D is: " << D << endl
-    << " numerator is: " << numerator << endl
-    << " back_N1plus_front is: " << back_N1plus_front << endl
-    << " Lower Order probability " << output << endl
-    << "------------------------------------------------" << endl;
+    cout
+        << "Lower Order" << endl
+        << " N1plus_front is: " << N1plus_front << endl
+        << " D is: " << D << endl
+        << " numerator is: " << numerator << endl
+        << " back_N1plus_front is: " << back_N1plus_front << endl
+        << " Lower Order probability " << output << endl
+        << "------------------------------------------------" << endl;
     return output;
 }
 
 template <class t_idx>
-double lowestorder(const t_idx& idx, 
-		   const uint64_t& pattern,
+double lowestorder(const t_idx& idx,
+                   const uint64_t& pattern,
                    uint64_t& lb_rev, uint64_t& rb_rev, uint64_t& char_pos, uint64_t& d)
 {
-    auto node = idx.m_cst_rev.node(lb_rev,rb_rev);
+    auto node = idx.m_cst_rev.node(lb_rev, rb_rev);
     double denominator = 0;
-    cout<<"SYMBOL="<<pattern<<" d="<<d<<endl;
+    cout << "SYMBOL=" << pattern << " d=" << d << endl;
     forward_search(idx.m_cst_rev, node, d, pattern, char_pos);
     d++;
     denominator = idx.m_N1plus_dotdot;
     lb_rev = idx.m_cst_rev.lb(node);
     rb_rev = idx.m_cst_rev.rb(node);
-    int numerator = N1PlusBack(idx, lb_rev, rb_rev, 1);//TODO precompute this
+    int numerator = N1PlusBack(idx, lb_rev, rb_rev, 1); //TODO precompute this
     double probability = (double)numerator / denominator;
 
-    cout 
-    << "Lowest Order numerator is: "<< numerator << endl 
-    << " denomiator is: " << denominator << endl 
-    << " Lowest Order probability " << probability << endl 
-    << "------------------------------------------------" << endl;
+    cout
+        << "Lowest Order numerator is: " << numerator << endl
+        << " denomiator is: " << denominator << endl
+        << " Lowest Order probability " << probability << endl
+        << "------------------------------------------------" << endl;
 
     return probability;
 }
 
 template <class t_idx>
-double pkn(const t_idx& idx, 
-	   const std::vector<uint64_t>::iterator& pattern_begin, 
+double pkn(const t_idx& idx,
+           const std::vector<uint64_t>::iterator& pattern_begin,
            const std::vector<uint64_t>::iterator& pattern_end,
-           const std::vector<uint64_t>::reverse_iterator& pattern_rbegin,
-	   const std::vector<uint64_t>::reverse_iterator& pattern_rend,
-	   uint64_t& lb, uint64_t& rb,
-           uint64_t &lb_rev,uint64_t &rb_rev, uint64_t& char_pos, uint64_t& d)
+           uint64_t& lb, uint64_t& rb,
+           uint64_t& lb_rev, uint64_t& rb_rev, uint64_t& char_pos, uint64_t& d)
 {
-    int size = std::distance(pattern_begin,pattern_end);
+    int size = std::distance(pattern_begin, pattern_end);
     double probability = 0;
     if ((size == ngramsize && ngramsize != 1) || (*pattern_begin == STARTTAG)) {
-	print(pattern_begin,pattern_end);
-        cout<<".("<<lb_rev<<","<<rb_rev<<")."<<endl;
-        probability = highestorder(idx, pattern_begin, pattern_end, 
-                                   pattern_rbegin, pattern_rend,
-				   lb, rb, 
-				   lb_rev, rb_rev, char_pos, d);
-        cout<<".("<<lb_rev<<","<<rb_rev<<")."<<endl;
+        print(pattern_begin, pattern_end);
+        cout << ".(" << lb_rev << "," << rb_rev << ")." << endl;
+        probability = highestorder(idx, pattern_begin, pattern_end,
+                                   lb, rb,
+                                   lb_rev, rb_rev, char_pos, d);
+        cout << ".(" << lb_rev << "," << rb_rev << ")." << endl;
     } else if (size < ngramsize && size != 1) {
-        print(pattern_begin,pattern_end);
-        if(size==0)exit(1);
-        cout<<"..("<<lb_rev<<","<<rb_rev<<").."<<endl;
+        print(pattern_begin, pattern_end);
+        if (size == 0)
+            exit(1);
+        cout << "..(" << lb_rev << "," << rb_rev << ").." << endl;
 
         probability = lowerorder(idx, pattern_begin, pattern_end,
-                                 pattern_rbegin, pattern_rend,
-				 lb, rb,
-				 lb_rev, rb_rev, char_pos, d);
+                                 lb, rb,
+                                 lb_rev, rb_rev, char_pos, d);
 
-        cout<<"..("<<lb_rev<<","<<rb_rev<<").."<<endl;
+        cout << "..(" << lb_rev << "," << rb_rev << ").." << endl;
     } else if (size == 1 || ngramsize == 1) {
-        cout<<"...("<<lb_rev<<","<<rb_rev<<")..."<<endl;
-        probability = lowestorder(idx, *(pattern_end-1), 
-			  	  lb_rev, rb_rev, char_pos, d);
-        cout<<"...("<<lb_rev<<","<<rb_rev<<")..."<<endl;
+        cout << "...(" << lb_rev << "," << rb_rev << ")..." << endl;
+        probability = lowestorder(idx, *(pattern_end - 1),
+                                  lb_rev, rb_rev, char_pos, d);
+        cout << "...(" << lb_rev << "," << rb_rev << ")..." << endl;
     }
     return probability;
 }
@@ -394,12 +376,11 @@ double run_query_knm(const t_idx& idx, const std::vector<uint64_t>& word_vec)
             cout << pattern[i] << " ";
         cout << endl;
 
-        uint64_t lb_rev = 0, rb_rev = idx.m_cst_rev.size() - 1, lb=0, rb=idx.m_cst.size() - 1;
-        uint64_t char_pos=0, d=0;
-        double score = pkn(idx, pattern.begin(),pattern.end(), 
-			   pattern.rend(), pattern.rbegin(),
-			   lb, rb, 
-			   lb_rev, rb_rev, char_pos, d);
+        uint64_t lb_rev = 0, rb_rev = idx.m_cst_rev.size() - 1, lb = 0, rb = idx.m_cst.size() - 1;
+        uint64_t char_pos = 0, d = 0;
+        double score = pkn(idx, pattern.begin(), pattern.end(),
+                           lb, rb,
+                           lb_rev, rb_rev, char_pos, d);
         final_score += log10(score);
         cout << "-----------------------------------------------" << endl;
     }
@@ -538,4 +519,3 @@ int main(int argc, const char* argv[])
     }
     return 0;
 }
-
