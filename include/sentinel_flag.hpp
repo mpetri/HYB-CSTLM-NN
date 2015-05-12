06261fecb95e27370c1df2b5240bac053386754b
Trevor Cohn <tcohn@unimelb.edu.au>
2015-05-12 12:51:33 +1000

    Made sentinel flag storage a bit faster, and store the offset into the edge where the first instance of the sentinel is located.

--- include/sentinel_flag.hpp
+++ include/sentinel_flag.hpp
@@ -14,10 +14,12 @@ struct compressed_sentinel_flag
 {
     typedef sdsl::int_vector<>::size_type size_type;
     typedef sdsl::rrr_vector<63> bv_type;
+    typedef sdsl::vlc_vector<> vector_type;
 
 private:
     bv_type m_bv;
     bv_type::rank_1_type m_bv_rank;
+    vector_type m_offsets;
 
 public:
 
@@ -27,47 +29,71 @@ public:
     compressed_sentinel_flag(t_cst& cst)
     {
         sdsl::bit_vector has_sentinel(cst.nodes());
+    	std::vector<uint32_t> offsets;
 
         // use the DFS iterator to traverse `cst`
         for (auto it=cst.begin(); it!=cst.end(); ++it) 
         {
             if (it.visit() == 1) 
-            {   // node visited the first time
-                auto node = *it;       // get the node by dereferencing the iterator           
+            {   
+                // all O(1) ops
+                auto node = *it;
                 auto node_id = cst.id(node);
                 auto parent = cst.parent(node);
-                auto parent_depth = cst.depth(node);
+                auto parent_id = cst.id(parent);
 
-                // check if edge contains sentinel
-                auto depth = cst.depth(node); // can be expensive for leaves
-                bool seen = false;
-
-                if (depth == parent_depth + 1 && it != cst.begin(parent)) 
+                if (has_sentinel[parent_id]) 
                 {
+                    // if a parent contains a sentinel then we just flag this and move on
+                    has_sentinel[node_id] = 1;
+                    offsets.push_back(0);
+                }
+                else 
+                {
+                    // check if edge contains sentinel and where
+                    auto parent_depth = cst.depth(node);
+                    auto depth = cst.depth(node); // can be expensive for leaves, but we need this (don't we?)
+                    auto delta = depth - parent_depth;
+                    bool found = false;
+
                     // pass: the EOS is lexicographical first, so it must be 
-                    // left-most child when the edge label has only one atom
-                }
-                else
-                {
-                    for (auto d = parent_depth+1; !seen && d <= depth; ++d) 
+                    // left-most child when the edge label has only one atom 
+                    //if (depth == parent_depth + 1 && it != cst.begin(parent)) // pass
+
+                    for (auto d = 1; !found && d <= delta; ++d) 
                     {
-                        auto l = cst.edge(node, d);
-                        if (l == EOS_SYM) seen = true;
+                        auto l = cst.edge(node, parent_depth+d);
+                        if (l == EOS_SYM) {
+                            has_sentinel[node_id] = 1;
+                            counts.push_back(d);
+                            found = true;
+                        }
                     }
+
+                    if (!found) 
+                        has_sentinel[node_id] = 0;
                 }
-                has_sentinel[node_id] = seen;
-                // FIXME: could also compute n1..4, N1..4 in this pass
             }
         }
 
         m_bv = bv_type(has_sentinel);
+        m_offsets = vector_type(offsets);
     }
 
+    // returns true/false for whether edge has EOS label
+    // offset = 0 -> sentinel is in an ancestor edge
+    // offset = 1+ -> sentinel in edge at given offset from parent depth
     template<class t_cst,class t_node_type>
-    bool has_sentinel_edge(t_cst& cst,t_node_type node) const 
+    bool has_sentinel_edge(t_cst& cst,t_node_type node, uint32_t &offset) const 
     {
     	auto id = cst.id(node);
-    	return m_bv[id];
+    	if (m_bv[id]) {
+            auto rank_in_vec = m_bv_rank(id);
+            offset = m_offsets[rank_in_vec];
+            return true;
+        }
+        else
+            return false;
     }
 
     size_type serialize(std::ostream& out, sdsl::structure_tree_node* v = NULL, std::string name = "") const
@@ -75,6 +101,7 @@ public:
         sdsl::structure_tree_node* child = sdsl::structure_tree::add_child(v, name, sdsl::util::class_name(*this));
         size_type written_bytes = 0;
         written_bytes += sdsl::serialize(m_bv,out,child,"bv");
+        written_bytes += sdsl::serialize(m_offsets,out,child,"offsets");
         sdsl::structure_tree::add_size(child, written_bytes);
         return written_bytes;
     }
@@ -83,5 +110,6 @@ public:
     {
         sdsl::load(m_bv, in);
         m_bv_rank.load(in,&m_bv);
+        sdsl::load(m_offsets, in);
     }
 };
