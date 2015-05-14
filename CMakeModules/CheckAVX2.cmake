2be8b1708301c74cf0afdfa2d0aba97507db267e
Matthias Petri <Matthias.Petri@gmail.com>
2015-05-14 10:00:16 +1000

    added fast popcnt detection

--- CMakeModules/CheckAVX2.cmake
+++ CMakeModules/CheckAVX2.cmake
@@ -1,11 +1,15 @@
 
 set(BUILTIN_INTERLEAVE 0)
+set(BUILTIN_POPCNT 0)
 # Check if we are on a Linux system
 if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
 	# Use /proc/cpuinfo to get the information
 	file(STRINGS "/proc/cpuinfo" _cpuinfo)
 	if(_cpuinfo MATCHES "(avx2)|(bmi2)")
 		set(BUILTIN_INTERLEAVE 1)
+	endif()
+	if(_cpuinfo MATCHES "(popcnt)|(sse4_2)")
+		set(BUILTIN_POPCNT 1)
 	endif()
 endif()	
 	
