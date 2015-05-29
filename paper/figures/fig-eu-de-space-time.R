a8307f817e12ab1405bfca6023c6ee93b7780281
Matthias Petri <Matthias.Petri@gmail.com>
2015-05-29 17:35:18 +1000

    add space time eu_DE graph

---
+++ paper/figures/fig-eu-de-space-time.R
@@ -0,0 +1,86 @@
+
+require("ggplot2")
+require("scales")
+require("grid")
+require("gridExtra")
+require("MASS")
+require("scales")
+require("sitools")
+library(tikzDevice)
+library(stringr)
+library(plyr)
+latex_percent <- function (x) {
+    x <- plyr::round_any(x, scales:::precision(x)/100)
+    stringr::str_c(comma(x * 100), "\\%")
+}
+
+csvFile <- "fig-eu-de-space-time.csv"
+outfile <- "fig-eu-de-space-time.tex"
+
+# options(tikzLatexPackages = 
+#           c(paste("\\input{",getwd(),"/../packages.tex}\n\\input{",getwd(),"/../macros.tex}",sep="")
+#           ) 
+# )
+
+theme_complete_bw <- function(base_size = 12, base_family = "") {
+  theme(
+    line =               element_line(colour = "black", size = 0.5, linetype = 1,lineend = "butt"),
+    rect =               element_rect(fill = "white", colour = "black", size = 0.5, linetype = 1),
+    text =               element_text(family = base_family, face = "plain",colour = "black", size = base_size,hjust = 0.5, vjust = 0.5, angle = 0, lineheight = 0.9),
+    axis.text =          element_text(size = rel(0.6), colour = "black"),
+    axis.title.y =       element_text(size = rel(0.7),angle = 90, colour = "black",vjust = 0.2),
+    axis.title.x =       element_text(size = rel(0.7), colour = "black"),
+    strip.text =         element_text(size = rel(0.6)),
+    legend.background =  element_blank(),
+    legend.margin =      unit(0.0, "cm"),
+    legend.key =         element_blank(),
+    legend.key.height =  unit(0.3, "cm"),
+    legend.key.width =   unit(0.3, "cm"),
+    legend.text =        element_text(size = rel(0.5)),
+    legend.text.align =  0,
+    legend.title =       element_blank(),
+    legend.title.align = NULL,
+    legend.justification = "center",
+    legend.direction = "horizontal",
+    legend.box =         NULL,
+    legend.position = c(0.5,-0.3),
+    panel.background =   element_rect(fill = NA, colour = "grey", size = 1.3),
+    panel.border =       element_blank(),
+    panel.grid.major =   element_line(colour = "grey90", size = 0.7),
+    panel.grid.minor =   element_line(colour = "grey90", size = 0.3),
+    panel.margin =       unit(0.5, "lines"),
+    strip.background =   element_rect(fill = NA, colour = NA),
+    strip.text.x =       element_text(colour = "black", size = base_size * 0.6),
+    strip.text.y =       element_text(colour = "black", size = base_size * 0.6, angle = -90),
+    plot.background =    element_rect(colour = NA, fill = "white"),
+    plot.title =         element_text(size = base_size * 1.2),
+    plot.margin=         unit(c(0,1,3.5,-1),"mm"),
+    complete = TRUE
+  )
+}
+
+mf_labeller <- function(var, value){
+    value <- as.character(value)
+    if (var=="process") { 
+        value[value=="construction"] <- "Construction Cost"
+        value[value=="query"]   <- "Query Cost"
+    }
+    return(value)
+}
+
+d <- read.csv(csvFile,sep=";")
+d$space <- d$space * 1024*1024
+
+plot <- ggplot(d,aes(type,y=time,x=space,group=factor(method),colour=factor(method)))
+plot <- plot + geom_point(size=2)
+plot <- plot + facet_grid(~ process,labeller=mf_labeller)
+plot <- plot + scale_x_log10(name="Space Usage [bytes]",labels=f2si)
+plot <- plot + scale_y_log10(breaks=c(10,100,1000,10000),name="Time [sec]",labels=f2si)
+plot <- plot + annotation_logticks(sides = "lb")
+
+plot <- plot + theme_complete_bw()
+plot <- plot+ guides(colour = guide_legend(ncol = 4))
+
+tikz(outfile,width = 3, height = 2)
+print(plot)
+dev.off();
