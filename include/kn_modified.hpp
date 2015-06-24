e045f8dc13f15751005213fc6f6fd73a8607f59a
eehsan <ehsan.shareghi@gmail.com>
2015-06-24 21:08:22 +1000

    mkn seems matching srilm now

--- include/kn_modified.hpp
+++ include/kn_modified.hpp
@@ -80,7 +80,7 @@ double prob_mod_kneser_ney_dual(const t_idx& idx,
         }
             
         uint64_t n1, n2, n3p;
-         // if it's the unigram level, the gamma can be computed using
+         //if it's the unigram level, the gamma can be computed using
          // precomputed quantities N1(.), N2(.), N3+(.)
 	 if (i == 1 || ngramsize == 1) {
             n1 = idx.m_precomputed.N1_dot;
@@ -108,7 +108,7 @@ double prob_mod_kneser_ney_single(const t_idx& idx,
         t_pat_iter pattern_begin, t_pat_iter pattern_end, uint64_t ngramsize)
 {
     typedef typename t_idx::cst_type::node_type t_node;
-    double p = 1.0 / idx.m_vocab.size(); // p -- FIXME: should we subtract away sentinels?
+    double p = 1.0 / (idx.m_vocab.size()-4); // p -- FIXME: should we subtract away sentinels? //ehsan: not sure why -4 works! but it works!
     t_node node_incl = idx.m_cst.root(); // v_F^all matching the full pattern, including last item
     t_node node_excl = idx.m_cst.root(); // v_F     matching only the context, excluding last item
     size_t size = std::distance(pattern_begin, pattern_end);
@@ -122,11 +122,11 @@ double prob_mod_kneser_ney_single(const t_idx& idx,
 
         // update the two searches into the CST
         if (ok) {
-            //LOG(INFO)<<"**start is: "<<*start<<endl;
+            LOG(INFO)<<"**start is: "<<idx.m_vocab.id2token(*start)<<endl;
             ok = backward_search_wrapper(idx.m_cst, node_incl, *start);
         }
         if (i >= 2) {
-            //LOG(INFO)<<"*start is: "<<*start<<endl;
+            LOG(INFO)<<"*start is: "<<idx.m_vocab.id2token(*start)<<endl;
             if (backward_search_wrapper(idx.m_cst, node_excl, *start) <= 0)
                 break;
         }
@@ -138,40 +138,57 @@ double prob_mod_kneser_ney_single(const t_idx& idx,
         double c, d;
         if ((i == ngramsize && ngramsize != 1) || (*start == PAT_START_SYM) ) {
             c = (ok) ? idx.m_cst.size(node_incl) : 0;
-	    //LOG(INFO)<<"idx.m_cst.size(node_incl) is: "<<idx.m_cst.size(node_incl)<<endl;
+	    LOG(INFO)<<"idx.m_cst.size(node_incl) is: "<<idx.m_cst.size(node_incl)<<endl;
             d = idx.m_cst.size(node_excl);
-            //LOG(INFO)<<"Highest Level: "<<c<<endl;
+	    LOG(INFO)<<"denominator: "<<d<<endl;
+            LOG(INFO)<<"Highest Level: "<<c<<endl;
         } else if (i == 1 || ngramsize == 1) {
             c = (ok) ? idx.N1PlusBack_from_forward(node_incl, start, pattern_end) : 0;
             d = idx.m_precomputed.N1plus_dotdot;
-            //LOG(INFO)<<"Lowest Level: "<<c<<endl;
+            LOG(INFO)<<"denominator: "<<d<<endl;
+            LOG(INFO)<<"Lowest Level: "<<c<<endl;
         } else {
             c = (ok) ? idx.N1PlusBack_from_forward(node_incl, start, pattern_end) : 0;
             d = idx.N1PlusFrontBack_from_forward(node_excl, start, pattern_end - 1);
-            //LOG(INFO)<<"Lower Level: "<<c<<endl;
+            LOG(INFO)<<"denominator: "<<d<<endl;
+            LOG(INFO)<<"Lower Level: "<<c<<endl;
         }
 
         // update the running probability
         if (c == 1) {
-            //LOG(INFO)<<"D1 is: "<<D1<<endl;
+            LOG(INFO)<<"D1 is: "<<D1<<endl;
             c -= D1;
         } else if (c == 2) {
-            //LOG(INFO)<<"D2 is: "<<D2<<endl;
+            LOG(INFO)<<"D2 is: "<<D2<<endl;
             c -= D2; 
         } else if (c >= 3) {
-            //LOG(INFO)<<"D3p is: "<<D3p<<endl;
+            LOG(INFO)<<"D3p is: "<<D3p<<endl;
             c -= D3p;
         }
             
         uint64_t n1, n2, n3p;
-        idx.N123PlusFront(node_excl, start, pattern_end - 1, n1, n2, n3p);
+        //if it's the unigram level, the gamma can be computed using
+        // n1_cnt, n2_cnt, vocab_size
+	// have a look at ModKneserNey::lowerOrderWeight function of srilm in
+        // Discount.cc 
+        if (i == 1 || ngramsize == 1) {
+            n1 = idx.m_precomputed.n1_cnt[1];
+            n2 = idx.m_precomputed.n2_cnt[1];
+            n3p = (idx.vocab_size()-2)-(n1 + n2);
+        }else{
+            idx.N123PlusFront(node_excl, start, pattern_end - 1, n1, n2, n3p);
+        }
+
+        //idx.N123PlusFront(node_excl, start, pattern_end - 1, n1, n2, n3p);
         double gamma = D1 * n1 + D2 * n2 + D3p * n3p;
         p = (c + gamma * p) / d;
-        //LOG(INFO)<<"n1 = "<<n1<<" n2 = "<<n2<<" n3p = "<<n3p<<endl;
-        //LOG(INFO)<<"gamma = "<<gamma<<endl;
-        //LOG(INFO)<<"Pattern is: "<<std::vector<u_int64_t>(pattern_begin,pattern_end);
-        //LOG(INFO)<<"probability is: "<<p<<endl;
-        //LOG(INFO)<<"----------------------------------"<<endl;
+        LOG(INFO)<<"n1 = "<<n1<<" n2 = "<<n2<<" n3p = "<<n3p<<endl;
+	LOG(INFO)<<"D1 = "<<D1<<" D2 = "<<D2<<" D3p = "<<D3p<<endl;
+        LOG(INFO)<<"gamma = "<<gamma/d<<" log10(gamma)= "<<log10(gamma/d)<<endl;
+	LOG(INFO) << "pattern is: " << idx.m_vocab.id2token(pattern_begin, pattern_end);
+        LOG(INFO)<<"Pattern is: "<<std::vector<u_int64_t>(pattern_begin,pattern_end);
+        LOG(INFO)<<"probability is: "<<p<<" log10(probability) is: "<<log10(p)<<endl;
+        LOG(INFO)<<"----------------------------------"<<endl;
     }
 
     return p;
