299dcac0a2d2e1779408187aa171e9aaebc16d07
Matthias Petri <Matthias.Petri@gmail.com>
2015-12-09 16:18:55 +1100

    special rare word token added

--- src/create-collection.cpp
+++ src/create-collection.cpp
@@ -5,19 +5,22 @@
 #include "utils.hpp"
 #include "constants.hpp"
 #include "collection.hpp"
+#include "logging.hpp"
 
 typedef struct cmdargs {
     std::string input_file;
     std::string collection_dir;
     bool byte_alphabet;
+    uint64_t min_symbol_freq;
 } cmdargs_t;
 
 void print_usage(const char* program)
 {
-    fprintf(stdout, "%s -i <input file>\n", program);
+    fprintf(stdout, "%s -i <input file> -c <coldir> -t <thres>\n", program);
     fprintf(stdout, "where\n");
     fprintf(stdout, "  -i <input file>      : the input file.\n");
     fprintf(stdout, "  -c <collection dir>  : the collection dir.\n");
+    fprintf(stdout, "  -t <threshold>       : min symbol freq.\n");
     fprintf(stdout, "  -1                   : byte parsing.\n");
 };
 
@@ -28,6 +31,7 @@ cmdargs_t parse_args(int argc, const char* argv[])
     args.input_file = "";
     args.collection_dir = "";
     args.byte_alphabet = false;
+    args.min_symbol_freq = 0;
     while ((op = getopt(argc, (char* const*)argv, "i:c:1")) != -1) {
         switch (op) {
         case 'i':
@@ -35,6 +39,9 @@ cmdargs_t parse_args(int argc, const char* argv[])
             break;
         case 'c':
             args.collection_dir = optarg;
+            break;
+        case 't':
+            args.min_symbol_freq = std::strtoull(optarg,NULL,10);
             break;
         case '1':
             args.byte_alphabet = true;
@@ -69,7 +76,9 @@ parse_line(const std::string& line, bool byte)
 
 int main(int argc, const char* argv[])
 {
-    /* parse command line */
+    log::start_log(argc, argv);
+
+    LOG(INFO) << "parse command line";
     cmdargs_t args = parse_args(argc, argv);
 
     /* create collection dir */
@@ -77,10 +86,13 @@ int main(int argc, const char* argv[])
 
     /* (1) create dict */
     std::unordered_map<std::string, uint64_t> dict;
+    dict.max_load_factor(0.2);
     std::vector<std::pair<uint64_t, std::string> > dict_ids;
     size_t max_id = 0;
     {
+        LOG(INFO) << "tokenize text";
         std::unordered_map<std::string, uint64_t> tdict;
+        tdict.max_load_factor(0.2);
         std::ifstream ifs(args.input_file);
         std::string line;
         while (std::getline(ifs, line)) {
@@ -88,12 +100,25 @@ int main(int argc, const char* argv[])
             for (const auto& tok : line_tokens)
                 tdict[tok]++;
         }
-        /* sort by freq */
+        LOG(INFO) << "sort by freq";
         for (const auto& did : tdict) {
             dict_ids.emplace_back(did.second, did.first);
         }
         std::sort(dict_ids.begin(), dict_ids.end(), std::greater<std::pair<uint64_t, std::string> >());
-        /* create id mapping */
+
+        LOG(INFO) << "remove low freq (<"<<args.min_symbol_freq<<") symbols";
+        for(size_t i=0;i<dict_ids.size();i++) {
+            if(dict_ids[i].first < args.min_symbol_freq) {
+                LOG(INFO) << "initial sigma = " << dict_ids.size();
+                LOG(INFO) << "initial log2(sigma) = " << sdsl::bits::hi(dict_ids.size()) + 1;
+                LOG(INFO) << "new sigma = " << i+1;
+                LOG(INFO) << "new log2(sigma) = " << sdsl::bits::hi(i+1) + 1;
+                dict_ids.resize(i+1);
+                break;
+            }
+        }
+
+        LOG(INFO) << "create id mapping";
         uint64_t cur_id = NUM_SPECIAL_SYMS;
         for (const auto& did : dict_ids) {
             dict[did.second] = cur_id;
@@ -101,7 +126,7 @@ int main(int argc, const char* argv[])
             cur_id++;
         }
     }
-    /* (2) 2nd pass to transform the integers */
+    LOG(INFO) << "2nd pass to transform the integers";
     {
         auto buf = sdsl::write_out_buffer<0>::create(args.collection_dir + "/" + KEY_PREFIX + KEY_TEXT);
         auto int_width = sdsl::bits::hi(max_id) + 1;
@@ -109,26 +134,34 @@ int main(int argc, const char* argv[])
         std::ifstream ifs(args.input_file);
         std::string line;
         buf.push_back(EOS_SYM); // file starts with EOS_SYM
+        uint64_t num_non_freq_syms = 0;
         while (std::getline(ifs, line)) {
             buf.push_back(PAT_START_SYM); // line starts with PAT_START_SYM
             auto line_tokens = parse_line(line, args.byte_alphabet);
             for (const auto& tok : line_tokens) {
                 auto itr = dict.find(tok);
-                auto num = itr->second;
-                buf.push_back(num);
+                if(itr == dict.end()) {
+                    buf.push_back(NOT_FREQ_SYM);
+                    num_non_freq_syms++;
+                } else {
+                    auto num = itr->second;
+                    buf.push_back(num);
+                }
             }
             buf.push_back(PAT_END_SYM); // line ends with PAT_END_SYM
             buf.push_back(EOS_SYM);
         }
         { // include special 'UNK' sentence to ensure symbol included in CST
-            // buf.push_back(PAT_START_SYM);
             buf.push_back(UNKNOWN_SYM);
-            // buf.push_back(PAT_END_SYM);
+            buf.push_back(NOT_FREQ_SYM);
             buf.push_back(EOS_SYM);
         }
         buf.push_back(EOF_SYM);
+        LOG(INFO) << "text size = " << buf.size();
+        LOG(INFO) << "num_non_freq_syms = " << num_non_freq_syms;
+        LOG(INFO) << "non freq percent = " << 100.0 * ( (double) num_non_freq_syms / (double) buf.size());
     }
-    /* (3) write vocab file */
+    LOG(INFO) << "write vocab file";
     {
         std::ofstream ofs(args.collection_dir + "/" + KEY_PREFIX + KEY_VOCAB);
         // write special symbols
@@ -137,6 +170,7 @@ int main(int argc, const char* argv[])
         ofs << "<UNK> 2\n";
         ofs << "<S> 3\n";
         ofs << "</S> 4\n";
+        ofs << "<NOT_FREQ> 5\n";
         // write the real vocab
         uint64_t cur_id = NUM_SPECIAL_SYMS;
         for (const auto& did : dict_ids) {
