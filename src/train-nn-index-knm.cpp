41cbb08ce21329a9964e470a9c1b23f608f76507
Matthias Petri <Matthias.Petri@gmail.com>
2017-01-09 15:01:44 +1100

    added dynet dependency

---
+++ src/train-nn-index-knm.cpp
@@ -0,0 +1,103 @@
+#include "sdsl/suffix_arrays.hpp"
+#include "sdsl/suffix_trees.hpp"
+
+#include "index_types.hpp"
+#include "logging.hpp"
+#include "utils.hpp"
+
+#include "mem_monitor.hpp"
+
+#include "dynet/dynet.h"
+#include "dynet/expr.h"
+#include "dynet/gpu-ops.h"
+#include "dynet/nodes.h"
+#include "dynet/training.h"
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+using namespace dynet;
+using namespace dynet::expr;
+using namespace cstlm;
+
+typedef struct cmdargs {
+  std::string collection_dir;
+  bool use_mkn;
+} cmdargs_t;
+
+void print_usage(const char *program) {
+  fprintf(stdout, "%s -c <collection dir>\n", program);
+  fprintf(stdout, "where\n");
+  fprintf(stdout, "  -c <collection dir>  : the collection dir.\n");
+  fprintf(stdout, "  -m                   : use modified kneser ney.\n");
+  fprintf(stdout, "  -t <threads>         : limit the number of threads.\n");
+};
+
+cmdargs_t parse_args(int argc, const char *argv[]) {
+  cmdargs_t args;
+  int op;
+  args.collection_dir = "";
+  args.use_mkn = false;
+  while ((op = getopt(argc, (char *const *)argv, "c:dmt:")) != -1) {
+    switch (op) {
+    case 'c':
+      args.collection_dir = optarg;
+      break;
+    case 'm':
+      args.use_mkn = true;
+      break;
+    case 't':
+      num_cstlm_threads = std::atoi(optarg);
+      break;
+    }
+  }
+  if (args.collection_dir == "") {
+    LOG(FATAL) << "Missing command line parameters.";
+    print_usage(argv[0]);
+    exit(EXIT_FAILURE);
+  }
+  return args;
+}
+
+template <class t_idx> t_idx create_and_store(collection &col, bool use_mkn) {
+  using clock = std::chrono::high_resolution_clock;
+  auto start = clock::now();
+  t_idx idx(col, use_mkn);
+  auto stop = clock::now();
+  LOG(INFO) << "index construction in (s): "
+            << std::chrono::duration_cast<std::chrono::milliseconds>(stop -
+                                                                     start)
+                       .count() /
+                   1000.0f;
+  auto output_file =
+      col.path + "/index/index-" + sdsl::util::class_to_hash(idx) + ".sdsl";
+  std::ofstream ofs(output_file);
+  if (ofs.is_open()) {
+    LOG(INFO) << "writing index to file : " << output_file;
+    auto bytes = sdsl::serialize(idx, ofs);
+    LOG(INFO) << "index size : " << bytes / (1024 * 1024) << " MB";
+    sdsl::write_structure<sdsl::HTML_FORMAT>(idx, output_file + ".html");
+  } else {
+    LOG(FATAL) << "cannot write index to file : " << output_file;
+  }
+  return idx;
+}
+
+int main(int argc, char **argv) {
+  dynet::initialize(argc, argv);
+  enable_logging = true;
+
+  /* parse command line */
+  cmdargs_t args = parse_args(argc, (const char **)argv);
+
+  /* parse collection directory */
+  collection col(args.collection_dir);
+  auto cstlm = create_and_store<wordlm>(col, args.use_mkn);
+
+  ComputationGraph cg;
+
+  return 0;
+}
